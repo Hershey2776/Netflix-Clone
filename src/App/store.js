9fe1b310097ffe39abc262e7877d47c66a0504c1
Harsh <harsh@hershey.heyhershey.tech>
2022-07-31 06:18:19 +0530

    Removed stateSlice

--- src/App/store.js
+++ src/App/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
-import stateReducer from "../features/stateSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    state: stateReducer,
   },
 });
